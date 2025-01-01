50f48054adb508a976ca8ce58aae9b2639309840
SpaceSlime <141542849+SpaceySlime@users.noreply.github.com>
2025-01-01 20:26:36 +1100

    Add demon mode

---
+++ Keyboard Cat (Demon).py
@@ -0,0 +1,23 @@
+# Keyboard Cat
+# Hard Mode (Only press usable keys)
+from pynput.keyboard import Key, Controller
+import pyautogui
+import random
+import time
+Keyboard = Controller()
+
+pyautogui.screenshot("Test.png", region= [0, 0, 1920, 1080])
+
+Keys = [] #Bias forwards, duh.
+convertstring = "w a s d t"
+Keys = convertstring.split()
+Keys.append(Key.space)
+
+def PressKey(KeyCode, Time):
+    Keyboard.press(KeyCode,)
+    time.sleep(Time)
+    Keyboard.release(KeyCode)
+
+while True:
+    time.sleep(0.1)
+    PressKey(random.choice(Keys), 0.1)
