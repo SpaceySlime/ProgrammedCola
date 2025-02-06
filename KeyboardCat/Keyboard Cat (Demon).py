f9f339b437a04f2a29ac9abee7535bcbd7c7dc3b
SpaceSlime <141542849+SpaceySlime@users.noreply.github.com>
2025-02-06 18:45:42 +1100

    Fix (3/3)

--- KeyboardCat/Keyboard Cat (Demon).py
+++ KeyboardCat/Keyboard Cat (Demon).py
@@ -1,14 +1,11 @@
 # Keyboard Cat
 # Hard Mode (Only press usable keys)
 from pynput.keyboard import Key, Controller
-import pyautogui
 import random
 import time
 Keyboard = Controller()
 
-pyautogui.screenshot("Test.png", region= [0, 0, 1920, 1080])
-
-Keys = [] #Bias forwards, duh.
+Keys = []
 convertstring = "w a s d t"
 Keys = convertstring.split()
 Keys.append(Key.space)
